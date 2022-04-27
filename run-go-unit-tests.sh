246cc1c53e323f8b0bd9b8d0f7cb4cc1be0dfc12
Rozen <rozenberguriel@gmail.com>
2022-04-27 12:41:27 -0300

    Update run-go-unit-tests.sh

--- run-go-unit-tests.sh
+++ run-go-unit-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-FILES=$(go list ./api/...  | grep -v /vendor/)
+FILES=$(go list   | grep -v /vendor/)
 
-go test -tags=unit -timeout 30s -short -v ${FILES}
+go test ./api/...
 
 returncode=$?
 if [ $returncode -ne 0 ]; then
